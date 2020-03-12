bb52535d4197fb6b94af5b0da70ecda59cf0bc3d
ricsonl <ricsonluizv@gmail.com>
2020-03-12 14:44:59 -0300

    initial files, matrix class and add function

---
+++ js/sketch.js
@@ -0,0 +1,13 @@
+function setup(){
+    createCanvas(550,550);
+    background(0);
+
+    var mat1 = new Matrix(3,3);
+    var mat2 = new Matrix(3,3);
+    
+    Matrix.add(mat1, mat2);
+}
+
+function draw(){
+    
+}
